99d36665a44af7c879cfc5d3bfcbd30abb9c4202
Cooletijdgast <larsbrinker2000@gmail.com>
2023-04-12 19:02:28 +0200

    Added dotenv.

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Sound } from '../model/sound';
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 @Injectable({
   providedIn: 'root',
 })
 export class SoundService {
-  private soundURL = 'http://localhost:4000';
+  private soundURL = "http://86.89.142.164:4000";
+  // private soundPort = process.env['PORT']!;
   constructor(private http: HttpClient) {}
 
   public getSounds(): Observable<Sound[]> {
+    // console.log(this.soundURL + ':' + this.soundPort);
     return this.http.get<Sound[]>(this.soundURL);
   }
 }
